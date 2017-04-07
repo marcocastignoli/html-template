a559d8ef143e4808d7d40eca02d7a5d13a359869
Marco Castignoli <marco.castignoli@joint-tech.it>
2017-04-07 13:29:16 +0100

    Managed full screen owlcarousel

--- js/owlcarousel.js
+++ js/owlcarousel.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        
-    });
+
+    function owlSetImages(event) {
+        var element = event.target
+        var images = $(element).children()
+        images.each(function (index,e) {
+            var e = $(e)
+            var image = e.data().background
+            if (image){
+                e.css("background-image", "url('"+image+"')")
+                e.css("background-size", "cover")
+                e.css("background-position", "bottom")
+                e.css("height", "100%")
+            }
+        })
+    }
+
+    var $owl = $(".owl-carousel")
+
+    $owl.on('initialize.owl.carousel', function (event) {
+        owlSetImages(event)
+    })
+
+    $owl.on('initialized.owl.carousel', function (event) {
+        if ( $(event.target).data().fullHeight ){
+            $(".owl-item").css("height", "100vh")
+        }
+    })
+
+    $owl.on('resized.owl.carousel', function(event) {
+        owlSetImages(event)
+    })
+
+    $owl.owlCarousel({
+        items: 1,
+        pagination : true,
+    })
 });
